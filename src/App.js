999acaeb1c4ae6d8ec29befaa975672f777868bd
Rsja12 <asocarras10@gmail.com>
2020-05-18 13:37:15 -0400

    render welcome page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Router, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
+import HomePage from './components/HomePage'
 
 const App = () => {
     return (
         <div className='ui container'>
-            <Router>
-                
-            </Router>
+            <BrowserRouter>
+                <Route exact path='/' component={ HomePage } />
+            </BrowserRouter>
         </div>
     )
 }
