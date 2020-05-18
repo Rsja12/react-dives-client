8c50cfad4446bd4e64f685da7326cbfc6639355b
Rsja12 <asocarras10@gmail.com>
2020-05-18 14:20:42 -0400

    include bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
+
 import HomePage from './components/HomePage'
 
 const App = () => {
     return (
         <div className='ui container'>
             <BrowserRouter>
-                <Route exact path='/' component={ HomePage } />
+                <div>
+                    <Route exact path='/' component={ HomePage } />
+                </div>
             </BrowserRouter>
         </div>
     )
