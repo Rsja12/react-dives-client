8c7da6d903a5195e42cf5f80633d3391f6f53d38
Rsja12 <asocarras10@gmail.com>
2020-05-18 13:28:46 -0400

    create index and App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Router, Route } from 'react-router-dom'
+
+const App = () => {
+    return (
+        <div className='ui container'>
+            <Router>
+                
+            </Router>
+        </div>
+    )
+}
+
+export default App
