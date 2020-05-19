45cf1ce31767aa4d716d811bce50c34ff20a1bc1
Rsja12 <asocarras10@gmail.com>
2020-05-19 15:10:05 -0400

    update navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,18 +3,25 @@ import { Link } from 'react-router-dom'
 
 const NavBar = () => {
     return (
-        <ul className='nav justify-content-center'>
-            <li className='nav-item'>
-                <Link className='nav-link' to='/'>
-                    React Dives
-                </Link>
-            </li>
-            <li className='nav-item'>
-                <Link className='nav-link' to='/about'>
-                    About
-                </Link>
-            </li>
-        </ul>
+        <nav className='navbar'>
+            <ul className='nav'>
+                <li className='nav-item'>
+                    <Link className='navbar-brand' to='/'>
+                        React Dives
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link className='nav-link' to='/about'>
+                        About
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link className='nav-link' to='/about'>
+                        Sign In
+                    </Link>
+                </li>
+            </ul>
+        </nav>
     )
 }
 
