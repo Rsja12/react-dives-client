8a51b71b62546007800dc0e6af5765590cf65121
Rsja12 <asocarras10@gmail.com>
2020-05-19 13:44:04 -0400

    semi-restart

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-
 const HomePage = () => {
     return (
         <div>
-            <h1>Welcome</h1>
-            <p>This is the public home page</p>
+            Welcome Page
         </div>
     )
 }
