3f17980a68fd7fd9a0bfd024b2cdf7c34748d278
Rsja12 <asocarras10@gmail.com>
2020-05-20 16:38:24 -0400

    conditionally render items in navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,32 +1,60 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 import GoogleAuth from '../authentication/GoogleAuth'
 
-const NavBar = () => {
-    return (
-        <nav className='navbar bg-dark'>
-            <ul className='nav'>
+const NavBar = ({ isSignedIn }) => {
+
+    if ( isSignedIn ) {
+        return (
+            <nav className='navbar bg-dark'>
+                <ul className='nav'>
+                    <li className='nav-item'>
+                        <Link 
+                        className='navbar-brand text-white' 
+                        to='/'>
+                            React Dives
+                        </Link>
+                    </li>
+                </ul>
+                
+                <div className='nav justify-content-end'>
                 <li className='nav-item'>
-                    <Link 
-                    className='navbar-brand text-white' 
-                    to='/'>
-                        React Dives
-                    </Link>
-                </li>
-            </ul>
-            <ul className='nav justify-content-end'>
-                <li className='nav-item'>
-                    <Link 
-                    className='nav-link text-white' 
-                    to='/about'>
-                        About
-                    </Link>
-                </li>
-                <GoogleAuth />
-            </ul>
-        </nav>
-    )
+                        <Link 
+                        className='navbar-brand text-white' 
+                        to='/about'>
+                            About
+                        </Link>
+                    </li>
+                    <GoogleAuth />
+                </div>
+            </nav>
+        )
+    } else {
+        return (
+            <nav className='navbar bg-dark'>
+                <ul className='nav'>
+                    <li className='nav-item'>
+                        <Link 
+                        className='navbar-brand text-white' 
+                        to='/'>
+                            React Dives
+                        </Link>
+                    </li>
+                </ul>
+                
+                <div className='nav justify-content-end'>
+                    <GoogleAuth />
+                </div>
+            </nav>
+        ) 
+    }
+
 }
 
-export default NavBar
+const mapStateToProps = state => ({
+    isSignedIn: state.auth.isSignedIn
+})
+
+export default connect(mapStateToProps)(NavBar)
