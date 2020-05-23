d640f2bd5dc59de737a6726703ca942fb40f8196
Rsja12 <asocarras10@gmail.com>
2020-05-23 14:02:12 -0400

    create state and first input field for form

--- src/components/divelogs/Form.js
+++ src/components/divelogs/Form.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react'
 
 export class Form extends Component {
+
+    state = {
+        location: '',
+        divesite: '',
+        maxDepth: null,
+        visibility: null,
+        waterTemp: null,
+        weight: null,
+        bottomTime: null
+    }
+
     render() {
         return (
-            <div>
-                Form
+            <div className='form-container'>
+                <div className='row'>
+                    <div className='col-md-6'>
+                        <label htmlFor='location'>Location</label>
+                        <input 
+                            type='text'
+                            onChange={ this.handleTextInput }
+                            value={ location }
+                        />
+                    </div>
+                </div>
             </div>
         )
     }
 }
 
 export default Form
-Form
