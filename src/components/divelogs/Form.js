2d7746782b70036df122cff684d0ee9f6ec32efd
Rsja12 <asocarras10@gmail.com>
2020-05-23 15:32:57 -0400

    add form inputs

--- src/components/divelogs/Form.js
+++ src/components/divelogs/Form.js
@@ -19,16 +19,88 @@ export class Form extends Component {
 
         return (
             <div className='form-container'>
-                <div className='row'>
-                    <div className='col-md-6'>
-                        <label htmlFor='location'>Location</label>
-                        <input 
-                            type='text'
-                            onChange={ this.handleTextInput }
-                            value={ location }
-                        />
+                <form>
+
+                    <div className='row'>
+                        <div className='col-md-6'>
+                            <label htmlFor='location'>Location</label>
+                            <input 
+                                className='form-control'
+                                type='text'
+                                id='location'
+                                onChange={ this.handleTextInput }
+                                value={ location }
+                            />
+                        </div>
+                        <div className='col-md-6'>
+                            <label htmlFor='divesite'>Divesite</label>
+                            <input 
+                                className='form-control'
+                                type='text'
+                                id='divesite'
+                                onChange={ this.handleTextInput }
+                                value={ divesite }
+                            />
+                        </div>
                     </div>
-                </div>
+
+                    <div className='row'>
+                        <div className='col-md-6'>
+                            <label htmlFor='maxDepth'>maxDepth</label>
+                            <input 
+                                className='form-control'
+                                type='text'
+                                id='maxDepth'
+                                onChange={ this.handleTextInput }
+                                value={ maxDepth }
+                            />
+                        </div>
+                        <div className='col-md-6'>
+                            <label htmlFor='bottomtime'>Bottom Time</label>
+                            <input 
+                                className='form-control'
+                                type='text'
+                                id='bottomTime'
+                                onChange={ this.handleTextInput }
+                                value={ bottomTime }
+                            />
+                        </div>
+                    </div>
+
+                    <div className='row'>
+                        <div className='col-md-4'>
+                            <label htmlFor='visiblity'>Visiblity</label>
+                            <input 
+                                className='form-control'
+                                type='text'
+                                id='visibility'
+                                onChange={ this.handleTextInput }
+                                value={ visibility }
+                            />
+                        </div>
+                        <div className='col-md-4'>
+                            <label htmlFor='waterTemp'>Water Temp</label>
+                            <input 
+                                className='form-control'
+                                type='text'
+                                id='waterTemp'
+                                onChange={ this.handleTextInput }
+                                value={ waterTemp }
+                            />
+                        </div>
+                        <div className='col-md-4'>
+                            <label htmlFor='weight'>Weight</label>
+                            <input 
+                                className='form-control'
+                                type='text'
+                                id='weight'
+                                onChange={ this.handleTextInput }
+                                value={ weight }
+                            />
+                        </div>
+                    </div>
+
+                </form>
             </div>
         )
     }
