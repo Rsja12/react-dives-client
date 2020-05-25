f70c3efbac2508fe0dca820959f5e3ebdc335fcf
Rsja12 <asocarras10@gmail.com>
2020-05-25 16:27:46 -0400

    updates combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,8 +3,8 @@ import { combineReducers } from 'redux'
 import authReducer from './authReducer'
 import logReducer from './logReducer';
 
-
 export default combineReducers({
     auth: authReducer,
     logs: logReducer
 })
+
