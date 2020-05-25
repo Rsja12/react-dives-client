d4fae9e4ac841cbcd976d043a460137dbea17370
Rsja12 <asocarras10@gmail.com>
2020-05-25 17:36:29 -0400

    add CardLinks to dashboard

--- src/components/divelogs/Dashboard.js
+++ src/components/divelogs/Dashboard.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import CardLink from '../CardLink'
 
 export class Dashboard extends Component {
     render() {
         return (
             <div>
                 Dashboard
+                <div className='row'>
+                    <div className='col-md-6'>
+                        <CardLink title={ 'Log a Dive' } path={ '/logs/new' } />
+                    </div>
+                    <div className='col-md-6'>
+                        <CardLink title={ 'View All Logs' } path={ '/logs' } />
+                    </div>
+                </div>
             </div>
         )
     }
