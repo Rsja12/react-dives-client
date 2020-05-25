ae43bdf7364f42012a781e8c424d169347c3edef
Rsja12 <asocarras10@gmail.com>
2020-05-25 15:05:10 -0400

    create CRUD action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,12 +13,64 @@ export const signOut = () => {
     }
 }
 
+export const fetchLogs = () => {
+
+    return async dispatch => {
+        const response = await axios.get('/logs').data 
+
+        dispatch({
+            type: 'FETCH_LOGS',
+            payload: response
+        })
+    }
+}
+
+export const fetchLog = id => {
+    return async dispatch => {
+        const response = await axios.get(`/logs/${id}`)
+
+        dispatch({
+            type: 'FETCH_LOG',
+            payload: response
+        })
+    }
+}
+
 export const createLog = formValues => {
+
     return async dispatch => {
         const response = await axios.post('/logs', formValues).data
+
         dispatch({
             type: 'CREATE_LOG',
             payload: response
         })
     }
+
+}
+
+export const editLog = id => {
+
+    return async dispatch => {
+        const response = await axios.patch(`/logs/${id}`).data
+
+        dispatch({
+            type: 'EDIT_LOG',
+            payload: response
+        })
+    }
+
+}
+
+export const deleteLog = id => {
+
+    return async dispatch => {
+        await axios.delete(`/logs/${id}`)
+
+        dispatch({
+            type: 'DELETE_LOG',
+            payload: id
+        })
+    }
+    
 }
