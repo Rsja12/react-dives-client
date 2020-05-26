4796e64e2d93f20e2a495c49fc78a94d83188e76
Rsja12 <asocarras10@gmail.com>
2020-05-26 16:05:09 -0400

    create show page

--- src/components/divelogs/ShowLog.js
+++ src/components/divelogs/ShowLog.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { connect } from 'react-redux'
 
-const ShowLog = () => {
+const ShowLog = ({ log }) => {
     return (
-        <div>
-            Show
-        </div>
+        <Fragment>
+            <h2 className='text-center'>Dive Number { log.id } on { log.date }</h2>
+            <div className='center-container'>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Location: {log.location}</li>
+                    <li class="list-group-item">Divesite: {log.divesite}</li>
+                    <li class="list-group-item">Max Depth: {log.maxDepth} Ft.</li>
+                    <li class="list-group-item">Bottom Time: {log.bottomTime} Min.</li>
+                    <li class="list-group-item">Visibility: {log.visibility} Ft.</li>
+                    <li class="list-group-item">Water Temp: {log.waterTemp} F</li>
+                    <li class="list-group-item">Weight: {log.weight} Lbs.</li>
+                    <li class="list-group-item">Notes: {log.notes}</li>
+                </ul>
+            </div>
+        </Fragment>
     )
 }
 
-export default ShowLog
+const mapStateToProps = (state, ownProps) => ({
+    log: state.logs[ownProps.match.params.id]
+})
+
+export default connect(mapStateToProps)(ShowLog)
