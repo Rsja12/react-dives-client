a001d485a6d085aec8ec4027fdc051ad2ae56ed7
Rsja12 <asocarras10@gmail.com>
2020-07-10 14:46:42 -0400

    refactor LogsIndex

--- src/components/divelogs/LogsIndex.js
+++ src/components/divelogs/LogsIndex.js
@@ -1,3 +1,48 @@
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+
+import { fetchLogs } from '../../actions'
+import LogItem from './LogItem'
+import AddDiveBtn from '../AddDiveBtn'
+
+const LogsIndex = props => {
+    
+    useEffect(() => {
+        const token = props.token
+        props.fetchLogs(token)
+    })
+    
+    const divelogs = props.logs.map(log => {
+        return(
+            <div className='center-container' key={log.id}>
+                <LogItem log={log} />
+            </div>
+        )
+    })
+    
+    if (!props.logs) return <div>Loading...</div>
+    return (
+        <div>
+            <AddDiveBtn />
+            {divelogs}
+        </div>
+    )
+}
+
+const mapStateToProps = state => ({
+    logs: Object.values( state.logs ),
+    token: state.auth.isSignedIn
+})
+
+export default connect(mapStateToProps, { fetchLogs })(LogsIndex)
+
+
+
+
+
+
+
+
 // import React, { Component, Fragment } from 'react'
 // import { connect } from 'react-redux'
 
@@ -39,52 +84,3 @@
 // })
 
 // export default connect(mapStateToProps, { fetchLogs })(LogsIndex)
-
-
-import React, { useEffect } from 'react'
-import { connect } from 'react-redux'
-
-import { fetchLogs } from '../../actions'
-import LogItem from './LogItem'
-import AddDiveBtn from '../AddDiveBtn'
-
-const LogsIndex = props => {
-
-    useEffect(() => {
-        const token = props.token
-        props.fetchLogs(token)
-    })
-
-
-    // How to have helper functions??????
-    // function renderLogs() {
-    //     props.logs.map(divelog => {
-    //         return (
-    //             <div className='center-container'>
-    //                 <LogItem log={divelog} />
-    //             </div>
-    //         )
-    //     })
-    // }   
-
-    if (!props.logs) return <div>Loading...</div>
-    return (
-        <div>
-            <AddDiveBtn />
-            {props.logs.map(log => {
-                return(
-                    <div className='center-container'>
-                        <LogItem log={log} />
-                    </div>
-                )
-            })}
-        </div>
-    )
-}
-
-const mapStateToProps = state => ({
-    logs: Object.values( state.logs ),
-    token: state.auth.isSignedIn
-})
-
-export default connect(mapStateToProps, { fetchLogs })(LogsIndex)
