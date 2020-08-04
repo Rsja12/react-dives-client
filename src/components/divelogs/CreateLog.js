3e79d84c608af3ca9cd7afb465f4208b343d2a6b
Rsja12 <asocarras10@gmail.com>
2020-08-04 19:27:50 -0400

    refactor CreateLog into functional component

--- src/components/divelogs/CreateLog.js
+++ src/components/divelogs/CreateLog.js
@@ -5,28 +5,26 @@ import { createLog } from '../../actions'
 import Form from './Form'
 import LinkButton from '../LinkButton'
 
-export class CreateLog extends Component {
+const CreateLog = ({ createLog }) => {
 
-    submit = formValues => {
+    const submit = formValues => {
         const token = localStorage.getItem('token')
-        this.props.createLog(formValues, token )
+        createLog(formValues, token )
     }
 
-    render() {
-        return (
-            <div>
-                <div className='top-row'>
-                    <LinkButton 
-                        className={'btn btn-outline-dark'}
-                        path={'/dashboard'} 
-                        text={'Back to all dives'} 
-                    />
-                </div>
-                <h2 className='text-center edit-page'>Add Another Dive!</h2>
-                <Form submit={ this.submit } />
+    return (
+        <div>
+            <div className='top-row'>
+                <LinkButton 
+                    className={'btn btn-outline-dark'}
+                    path={'/dashboard'} 
+                    text={'Back to all dives'} 
+                />
             </div>
-        )
-    }
+            <h2 className='text-center edit-page'>Add Another Dive!</h2>
+            <Form submit={ submit } />
+        </div>
+    )
 }
 
 export default connect(null, { createLog })(CreateLog)
