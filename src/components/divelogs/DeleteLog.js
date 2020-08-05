3e3143ffb4685f40c59947fadea8bb85179f4ca1
Rsja12 <asocarras10@gmail.com>
2020-08-04 20:05:28 -0400

    refactor EditLog into functional component with useEffect hook

--- src/components/divelogs/DeleteLog.js
+++ src/components/divelogs/DeleteLog.js
@@ -57,67 +57,6 @@ const DeleteLog = props => {
     )
 }
 
-// export class DeleteLog extends Component {
-
-//     state = {
-//         isOpen: true
-//     }
-
-//     componentDidMount() {
-//         const token = localStorage.getItem('token')
-//         const id = this.props.match.params.id
-//         this.props.fetchLog(token, id)
-//     }
-    
-//     hideModal = () => {
-//         this.setState({
-//             isOpen: false
-//         })
-//         const id = this.props.match.params.id
-//         history.push(`/logs/${id}`)
-//     }
-    
-//     handleSubmit = e => {
-//         e.preventDefault()
-        
-//         const token = localStorage.getItem('token')
-//         const id = this.props.match.params.id
-//         this.props.deleteLog(token, id)
-//     }
-
-//     render() {
-//         if (!this.props.log) {
-//             return <div>Loading...</div>
-//         }
-        
-//         return (
-//             <Modal 
-//             centered
-//             show={this.state.isOpen}
-//             onHide={this.hideModal}>
-//                 <Modal.Header>
-//                     <Modal.Title>{ this.props.log.divesite } - { this.props.log.location }</Modal.Title>
-//                 </Modal.Header>
-//                 <Modal.Body>
-//                     Are you sure? This will permanently delete this dive from your logbook
-//                 </Modal.Body>
-//                 <Modal.Footer>
-//                     <button 
-//                     onClick={ this.hideModal }
-//                     className='btn btn-dark'>
-//                         Cancel
-//                     </button>
-//                     <button 
-//                     onClick={ this.handleSubmit }
-//                     className='btn btn-danger'>
-//                         Delete
-//                     </button>
-//                 </Modal.Footer>
-//             </Modal>
-//         )
-//     }
-// }
-
 const mapStateToProps = (state, ownProps) => ({
     log: state.logs[ownProps.match.params.id]
 })
--- src/components/divelogs/EditLog.js
+++ src/components/divelogs/EditLog.js
@@ -1,53 +1,47 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 
 import { fetchLog, editLog } from '../../actions'
 import Form from './Form'
 import LinkButton from '../LinkButton'
 
-export class EditLog extends Component {
+const EditLog = props => {
 
-    componentDidMount() {
-        const token = localStorage.getItem('token')
-        const id = this.props.match.params.id
-        this.props.fetchLog(token, id)
-    }
-    
-    submit = formValues => {
-        const token = localStorage.getItem('token')
-        const id = this.props.match.params.id
-        this.props.editLog(id, formValues, token)
+    const token = localStorage.getItem('token')
+    const id = props.match.params.id
+
+    useEffect(() => {
+        props.fetchLog(token, id)
+    }, [])
+
+    const submit = formValues => {
+        props.editLog(id, formValues, token)
     }
 
-    render() {
-        if (!this.props.log) {
-            return <div>Loading...</div>
-        }
-        const { location, divesite, max_depth, bottom_time, visibility, water_temp, weight, notes, id } = this.props.log
-        return (
-            <div>
-                <div className='top-row'>
-                    <LinkButton 
-                        className={'btn btn-outline-dark'}
-                        path={`/logs/${id}`} 
-                        text={'Nevermind'} 
-                    />
-                </div>
-                <h2 className='text-center edit-page'>Edit This Dive</h2>
-                <Form
-                    location={ location }
-                    divesite={ divesite }
-                    max_depth={ max_depth }
-                    bottom_time={ bottom_time }
-                    visibility={ visibility }
-                    water_temp={ water_temp }
-                    weight={ weight }
-                    notes={ notes }
-                    submit={ this.submit }
+    if (!props.log) return <div>Loading...</div>
+    return (
+        <div>
+            <div className='top-row'>
+                <LinkButton 
+                className={'btn btn-outline-dark'}
+                path={`/logs/${id}`} 
+                text={'Nevermind'} 
                 />
             </div>
-        )
-    }
+            <h2 className='text-center edit-page'>Edit This Dive</h2>
+            <Form
+                location={ props.log.location }
+                divesite={ props.log.divesite }
+                max_depth={ props.log.max_depth }
+                bottom_time={ props.log.bottom_time }
+                visibility={ props.log.visibility }
+                water_temp={ props.log.water_temp }
+                weight={ props.log.weight }
+                notes={ props.log.notes }
+                submit={ submit }
+            />
+        </div>
+    )
 }
 
 const mapStateToProps = (state, ownProps) => ({
