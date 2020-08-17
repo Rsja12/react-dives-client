67ce6575602958e2e458012ffb389433249d244e
Rsja12 <asocarras10@gmail.com>
2020-08-17 19:22:36 -0400

    add three unit tests to App component

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Route } from 'react-router-dom';
+
+import App from '../App';
+import NavBar from '../components/NavBar';
+import PrivateRoute from '../routes/PrivateRoute';
+import PublicRoute from '../routes/PublicRoute';
+
+let wrapper;
+
+beforeEach(() => {
+    wrapper = shallow(<App />);
+});
+
+it ('renders without crashing', () => {
+    expect(wrapper).toMatchSnapshot();
+});
+
+it ('renders 1 NavBar component', () => {
+    expect(wrapper.find(NavBar).length).toEqual(1);
+});
+
+it ('renders 3 PublicRoute components', () => {
+    expect(wrapper.find(PublicRoute).length).toEqual(3);
+});
